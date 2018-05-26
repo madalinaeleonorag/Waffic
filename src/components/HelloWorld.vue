591a825084472583a91b73920f75bee7b31419ad
madalinaeleonrag <madalinaeleonora.gheorghe@gmail.com>
2018-05-26 16:09:53 +0300

    before errors with dependences

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,6 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <template>
-          <v-parallax :src="require('../assets/HelloWorld/accountdetails.PNG')"></v-parallax>
-        </template>
         <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
       </v-layout>
     </v-slide-y-transition>
